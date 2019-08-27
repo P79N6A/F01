e1ebc5ed0f62a86fa8f15036a54130c22bf509e7
admin <849940845@qq.com>
2019-08-27 18:45:11 +0800

    UI2

--- assets/Script/Initial.js
+++ assets/Script/Initial.js
@@ -7,12 +7,18 @@ cc.Class({
             type: cc.Label
         },
         // defaults, set visually when attaching this script to the Canvas
-        text: 'Hello, World!'
+        text: 'Hello, World!',
+        window:{
+            default: null,
+            type: cc.Node,
+        },
     },
 
     // use this for initialization
     onLoad: function () {
         this.label.string = this.text;
+        //console.log(this.window);
+        this.window.active = false;
     },
 
     // called every frame
